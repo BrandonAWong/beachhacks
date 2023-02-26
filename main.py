e6d543939f2e381bbf8f84f16cab7d3a1b4b2b10
BrandonAWong <brandon.alex.wong@gmail.com>
2023-02-25 20:48:31 -0800

    extracted emotion code into emotion.py

--- main.py
+++ main.py
@@ -1,23 +1,42 @@
-from deepface import DeepFace
-import cv2
+from Emotion import Emotion
+from tkinter import *
+from PIL import ImageTk, Image
+from time import sleep
+import os
 
-vid = cv2.VideoCapture(0)
+def tki():
+    # Create an instance of tkinter window
+    win = Tk()
+    # Define the geometry of the window
+    win.geometry("700x500")
 
-while(True):
-    ret, frame = vid.read()
-    cv2.imshow('frame', frame)
-    image = vid.read()
-    cv2.imwrite('img.png', frame)
-    try:
-        face_analysis = DeepFace.analyze(img_path = "img.png")
-        print(face_analysis[0]['dominant_emotion'])
-    except ValueError as e:
-        print(e)
-        pass
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-  
-vid.release()
-cv2.destroyAllWindows()
+    frame = Frame(win, width=600, height=400)
+    frame.pack()
+    frame.place(anchor='center', relx=0.5, rely=0.5)
+    wd = f'{os.getcwd()}\\catt'
+    print(emotion.emotion)
+    match emotion.emotion:
+        case('happy'):
+            file_name = f"{wd}\\happy_cat.bmp"
+        case('sad'):
+            file_name = f"{wd}\\sad_cat.bmp"
+        case('angry'):
+            file_name = f"{wd}\\angry_cat.bmp"
+        case('neutral'):
+            file_name = f"{wd}\\neutral_cat.bmp"
+        case('disgusted'):
+            file_name = f"{wd}\\disgusted_cat.bmp"
+    print(file_name)
+    img = ImageTk.PhotoImage(Image.open(file_name))
+
+    # Create a Label Widget to display the text or Image
+    label = Label(frame, image = img)
+    label.pack()
+
+    win.mainloop()
+
+emotion = Emotion()
+emotion.capture_emotion()
+tki()
 
 
