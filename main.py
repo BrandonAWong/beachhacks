3515dda486a663ccb2828bbae5269304fc98170c
BrandonAWong <brandon.alex.wong@gmail.com>
2023-02-26 10:10:43 -0800

    only using tkinter now, connected to dialogflow

--- main.py
+++ main.py
@@ -1,20 +1,42 @@
 from Emotion import Emotion
 from tkinter import *
 from PIL import ImageTk, Image
+from os import getcwd
 from time import sleep
-import os
+from google.cloud import dialogflow_v2 as dialogflow
+from google.api_core.exceptions import InvalidArgument
 
-def tki():
-    # Create an instance of tkinter window
+DIALOGFLOW_PROJECT_ID = 'beachhacks-378923'
+DIALOGFLOW_LANGUAGE = "en-US"
+SESSION_ID = "123456789"
+sessionClient = dialogflow.SessionsClient()
+session = sessionClient.session_path(DIALOGFLOW_PROJECT_ID, SESSION_ID)
+
+def start():
     win = Tk()
-    # Define the geometry of the window
-    win.geometry("700x500")
+    win.title('Cat Therapy')
+    win.geometry("1000x700")
 
     frame = Frame(win, width=600, height=400)
     frame.pack()
     frame.place(anchor='center', relx=0.5, rely=0.5)
-    wd = f'{os.getcwd()}\\catt'
-    print(emotion.emotion)
+
+    wd = f'{getcwd()}\\catt'
+    file_name = f'{wd}\\loading_cat.bmp'
+    img = ImageTk.PhotoImage(Image.open(file_name))
+    imgLabel = Label(frame, image = img)
+    imgLabel.pack()
+    textLabel = Label(frame, text="stand still I'm capturing your emotions!")
+    textLabel.pack()  
+    win.update()
+
+    emotion.capture_emotion()
+    textLabel['text'] = 'picking the perfect emotion...'
+    img = ImageTk.PhotoImage(Image.open('img.png'))
+    imgLabel['image'] = img
+    win.update()
+    sleep(3)
+
     match emotion.emotion:
         case('happy'):
             file_name = f"{wd}\\happy_cat.bmp"
@@ -26,17 +48,42 @@ def tki():
             file_name = f"{wd}\\neutral_cat.bmp"
         case('disgusted'):
             file_name = f"{wd}\\disgusted_cat.bmp"
-    print(file_name)
+        case('fear'):
+            file_name = f"{wd}\\fear_cat.bmp"
+        case('surprise'):
+            file_name = f"{wd}\\suprise_cat.bmp"
+
     img = ImageTk.PhotoImage(Image.open(file_name))
-
-    # Create a Label Widget to display the text or Image
-    label = Label(frame, image = img)
-    label.pack()
-
+    imgLabel['image'] = img
+    text = f"i'm {emotion.emotion}"
+    win.update()
+    dialog = start_dialog(text)
+    textLabel['text'] = ''
+    for letter in dialog:
+        textLabel['text'] += letter
+        win.update()
+        sleep(0.05)
+    userInputBox = Entry(frame)
+    userInputBox.pack()
+    userInput = userInputBox.get()
+    #LEFT OFF HERE NEED A BUTTON TO SUBMIT TEXT
     win.mainloop()
 
-emotion = Emotion()
-emotion.capture_emotion()
-tki()
+def start_dialog(text):
+    textInput = dialogflow.types.TextInput(text=text, language_code=DIALOGFLOW_LANGUAGE)
+    query = dialogflow.types.QueryInput(text=textInput)
+    try:
+        response = sessionClient.detect_intent(session=session, query_input=query)
+    except InvalidArgument:
+        raise 
+    print("query:", response.query_result.query_text)
+    print("Detected intent:", response.query_result.intent.display_name)
+    print("Detected intent confidence:", response.query_result.intent_detection_confidence)
+    print("Fulfillment text:", response.query_result.fulfillment_text)
+    return response.query_result.fulfillment_text
 
 
+if __name__ == "__main__":
+    emotion = Emotion()
+    #add button make text nicer and stuff and make a go go button at very beginning
+    start()
